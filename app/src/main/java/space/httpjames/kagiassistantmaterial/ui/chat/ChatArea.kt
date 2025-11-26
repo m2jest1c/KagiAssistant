638d80fa842c4fff0d41cee08bf1f86f3a3b73ad
httpjamesm <github@httpjames.space>
2025-11-25 19:59:32 -0500

    style: alpha on placeholder

--- app/src/main/java/space/httpjames/kagiassistantmaterial/ui/chat/ChatArea.kt
+++ app/src/main/java/space/httpjames/kagiassistantmaterial/ui/chat/ChatArea.kt
@@ -4,9 +4,7 @@ import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -25,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -108,12 +107,13 @@ fun ChatArea(
                         tint = Color.Unspecified,
                         modifier = Modifier
                             .padding(12.dp)
-                            .size(96.dp),
+                            .size(96.dp)
+                            .alpha(0.6f),
                     )
-                    Spacer(modifier = Modifier.height(16.dp))
                     Text(
                         "Kagi Assistant",
                         style = MaterialTheme.typography.displaySmall,
+                        modifier = Modifier.alpha(0.6f)
                     )
                 }
             }
