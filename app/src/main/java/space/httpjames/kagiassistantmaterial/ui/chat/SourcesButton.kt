b31a4fd20879aa0b64ef79dcae61e16666db9aa6
httpjamesm <github@httpjames.space>
2025-11-27 00:14:11 -0500

    feat: white backgrounds for source icons

--- app/src/main/java/space/httpjames/kagiassistantmaterial/ui/chat/SourcesButton.kt
+++ app/src/main/java/space/httpjames/kagiassistantmaterial/ui/chat/SourcesButton.kt
@@ -1,5 +1,6 @@
 package space.httpjames.kagiassistantmaterial.ui.chat
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -13,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
@@ -57,6 +59,7 @@ fun SourcesButton(
                         .offset(x = iconSize * index * 0.6f) // overlap by ~40 %
                         .zIndex((domains.size - index).toFloat()) // right-most on top
                         .clip(CircleShape)
+                        .background(Color.White),
                 )
             }
         }
