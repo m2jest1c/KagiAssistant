564d364dd34c4ca58f29fd0c546c3e9804dd2c15
httpjamesm <github@httpjames.space>
2025-12-20 02:15:33 -0500

    chore: 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "space.httpjames.kagiassistantmaterial"
         minSdk = 36
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
