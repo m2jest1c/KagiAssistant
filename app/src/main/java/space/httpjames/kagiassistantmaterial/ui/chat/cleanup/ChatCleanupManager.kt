adbb76d2d88a4276bd04370529c7b14f82204648
httpjamesm <github@httpjames.space>
2025-12-20 01:08:16 -0500

    feat: only run cleanup if online

--- app/src/main/java/space/httpjames/kagiassistantmaterial/ui/chat/cleanup/ChatCleanupManager.kt
+++ app/src/main/java/space/httpjames/kagiassistantmaterial/ui/chat/cleanup/ChatCleanupManager.kt
@@ -1,7 +1,9 @@
 package space.httpjames.kagiassistantmaterial.ui.chat.cleanup
 
 import android.content.Context
+import androidx.work.Constraints
 import androidx.work.ExistingWorkPolicy
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.workDataOf
@@ -14,8 +16,14 @@ object ChatCleanupManager {
             "SESSION_TOKEN" to sessionToken
         )
 
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED) // this is obviously a network request so no use to do it offline
+            .build()
+
+        // the exponential backoff will work here if we go offline
         val workRequest = OneTimeWorkRequestBuilder<ChatCleanupWorker>()
             .setInitialDelay(15, TimeUnit.MINUTES)
+            .setConstraints(constraints)
             .setInputData(data)
             .build()
 
