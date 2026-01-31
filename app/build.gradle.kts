a026e87a7db88c99a69fe641616a6942ed7bb612
httpjamesm <github@httpjames.space>
2026-01-31 14:09:45 -0500

    docs: 1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "space.httpjames.kagiassistantmaterial"
         minSdk = 33
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
